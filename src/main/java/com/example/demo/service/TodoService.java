dfad1d38d7cd25bf62549e1d248b299b0f67625d
Jhee Seo In <banny19@naver.com>
2023-06-16 23:03:32 +0900

    백엔드 개발 17. @Repository에서 쿼리 추가하기

--- src/main/java/com/example/demo/service/TodoService.java
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -8,18 +8,19 @@ import com.example.demo.persistence.TodoRepository;
 
 @Service
 public class TodoService {
-
+	
 		@Autowired
 		private TodoRepository repository;
 		
 		public String testService(){
 				// Todo Entity 생성
-				TodoEntity entity = TodoEntity.builder().title("My first todo item").build();
+				TodoEntity entity = TodoEntity.builder().userId("user01").title("My first todo item").build();
 				// Todo Entity 저장
 				repository.save(entity);
 				// Todo Entity 검색
-				TodoEntity savedEntity = repository.findById(entity.getId()).get();
-				return savedEntity.getTitle();
+				TodoEntity savedEntity = 
+repository.findByUserId(entity.getUserId()).get(0);
+				return savedEntity.getUserId();
 				
-		}
+	}
 }
